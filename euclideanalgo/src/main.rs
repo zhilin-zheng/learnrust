f3a6cc92c29d4605b4912fbd2544d939a2b7d968
Zhilin Zheng <zhengzhilin2010@gmail.com>
2025-01-04 09:46:46 +0100

    take command line arguments

--- euclideanalgo/src/main.rs
+++ euclideanalgo/src/main.rs
@@ -1,3 +1,6 @@
+use std::env;
+use std::str::FromStr;
+
 fn gcd(mut n: u64, mut m: u64) -> u64 {
     assert!(n != 0 && m != 0);
     while m != 0 {
@@ -11,13 +14,26 @@ fn gcd(mut n: u64, mut m: u64) -> u64 {
     n
 }
 fn main() {
-    let x: u64 = 4;
-    let y: u64 = 6;
-    let divisor = gcd(x, y);
-    println!(
+    let mut numbers = Vec::new();
+    for arg in env::args().skip(1) {
+        numbers.push(u64::from_str(&arg).expect("error parsing argument"));
+    }
+    if numbers.len() == 0 {
+        eprintln!("Usage: gcd NUMBER ...");
+        std::process::exit(1);
+    }
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+    println!("The greatest common divisor of {:?} is {}", numbers, d)
+    // let x: u64 = 4;
+    // let y: u64 = 6;
+    // let divisor = gcd(x, y);
+    /*println!(
         "The greatest common divisor of {} and {} is: {}\n",
         x, y, divisor
-    );
+    );*/
 }
 #[test]
 fn test_gcd() {
